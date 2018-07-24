2f5a6230666e7c035014fafafaf522841532aac7
Christopher Homer <34613515+cjh748@users.noreply.github.com>
2018-07-24 18:34:02 +0100

    Version 3

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,7 +13,15 @@ class SignUpForm(forms.ModelForm):
 
     class Meta:
         model = User
+        labels = {}
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
 
 class LogInForm(forms.Form):
